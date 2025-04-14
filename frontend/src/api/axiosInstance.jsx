061043f635875d21aa359233b76ac88b75e81d91
srlog <146011589+srlog@users.noreply.github.com>
2025-04-15 00:55:07 +0530

    Update axiosInstance.jsx

--- frontend/src/api/axiosInstance.jsx
+++ frontend/src/api/axiosInstance.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:3000/api/",
+  baseURL: "https://0xr7z64z-3000.inc1.devtunnels.ms/api/",
 });
 
 axiosInstance.interceptors.request.use((config) => {
