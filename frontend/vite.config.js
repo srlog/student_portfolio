386bd24971f12d4d82c5518fe3c4abdd04eb938f
srlog <146011589+srlog@users.noreply.github.com>
2025-04-15 20:03:07 +0530

    adding fallback

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,16 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './',
+  publicDir: 'public',
+  server: {
+    // When hosting on Firebase, it expects a single page application
+    // and will serve the index.html for any route that doesn't match a file
+    // Vite will also do this when in development mode, so we don't need to
+    // specify this option. However, when hosting on Firebase, we need to
+    // specify this option so that Vite knows to serve the index.html for
+    // any route that doesn't match a file.
+    // https://vitejs.dev/config/#server-fallback
+    fallback: 'index.html',
+  },
 })
